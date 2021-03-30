359c9f0b391780c3d76e72c74a3a9a3ceb0f0508
kikeztw <kikeztw@gmail.com>
2021-03-29 22:02:26 -0400

    fix check

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ValidatorType = (data: any) => [boolean, string];
-
 export type RulesType<R extends string, P extends string> = {
   [key in R]: {
-      [key in P]:{
-        can: boolean | ValidatorType;
-        message?: string;
-      }
+    [key in P]: {
+      can?: boolean;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      validator?: (data: any) => [boolean, string];
+      message?: string;
+    };
   };
 };
 
-
-export type FunctionCheckType<R, P> = (role: R, permission: P, data?: any) => void
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type FunctionCheckType<R, P> = (
+  role: R,
+  permission: P,
+  data?: any,
+) => void;
--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,30 @@
 import { RulesType, FunctionCheckType } from './types';
 
 export const checkGenerator = <R extends string, P extends string>(
-  rules: RulesType<R, P>,
-): FunctionCheckType<R, P> => {
-  return (role: R, permissionName: P, data: any): [boolean, string] => {
+  rules: RulesType<R, P>, // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): FunctionCheckType<R, P> => (
+    role: R,
+    permissionName: P,
+    data: any,
+  ): [boolean, string] => {
     const currentRole = role ? rules[role] : null;
 
     if (!currentRole) {
-      console.error(
-        'Check error: the role could not be found within the rules',
-      );
-      return [
-        false,
-        'Check error: the role could not be found within the rules',
-      ];
+      console.error('Check error: the role could not be found within the rules');
+      return [false, 'Check error: the role could not be found within the rules'];
     }
 
     const permission = currentRole[permissionName];
     if (permission.can && typeof permission.can === 'boolean') {
       return [permission.can, permission?.message || ''];
-    } else if (permission.can && typeof permission.can === 'function') {
-      return permission.can(data);
+    }
+
+    if (permission.validator && typeof permission.validator === 'function') {
+      return permission.validator(data);
     }
 
     return [false, 'Not Permission found in rules'];
   };
-};
 
 /*
   enum test {
