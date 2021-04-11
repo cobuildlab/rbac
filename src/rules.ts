27f71cf072e83f01c27083a76a67a39ca0528a07
kikeztw <kikeztw@gmail.com>
2021-04-11 18:53:54 -0400

    added JSDOC

--- src/rules.ts
+++ src/rules.ts
@@ -8,26 +8,47 @@ export class Rules {
   private currentPermission = '';
   private currentRole = '';
 
+  /**
+   * @description - Check if the currently assigned role is within the rules.
+   */
   private checkCurrentRole(): void {
     if (!this.rules[this.current]) {
       throw new Error('Role not defined');
     }
   }
 
+  /**
+   * @description -  Check if the currently assigned permission is within the current role.
+   */
   private checkCurrentPermission(): void {
     if (!this.rules[this.current][this.currentPermission]) {
       throw new Error('Permssion not defined');
     }
   }
 
+  /**
+   * @description - Check if a role is withing the rules.
+   * @param role - Role to fined in rules.
+   * @returns {boolean} - Return true is find the role.
+   */
   private isRoleInRules(role: string): boolean {
     return Boolean(this.rules[role]);
   }
 
+  /**
+   * @description - Check if a permission is withing the a role.
+   * @param role - Role to fined in rules.
+   * @param permission - Permission to fined in the role.
+   * @returns {boolean} - Return true is find the permission.
+   */
   private isPermissionInRole(role: string, permission: string): boolean {
     return Boolean(this.rules[role]?.[permission]);
   }
 
+  /**
+   * @description - Configure a default role to avoid passing it every time in the check function.
+   * @param name - Role name.
+   */
   setDefaultRole(name: string): void {
     if (this.isRoleInRules(name)) {
       this.currentRole = name;
@@ -36,6 +57,11 @@ export class Rules {
     }
   }
 
+  /**
+   * @description - Added a role to rules.
+   * @param name - Role name.
+   * @returns {Rules} -
+   */
   role(name: string): this {
     if (!this.isRoleInRules(name)) {
       Object.assign(this.rules, { [name]: {} });
@@ -44,6 +70,11 @@ export class Rules {
     return this;
   }
 
+  /**
+   * @description - Added a permission to a role.
+   * @param name - Permission name.
+   * @returns {Rules} -
+   */
   permission(name: string): this {
     this.checkCurrentRole();
     if (!this.isPermissionInRole(this.current, name)) {
@@ -53,6 +84,12 @@ export class Rules {
     return this;
   }
 
+  /**
+   * @description - Used for added validation for static permission.
+   * @param status - Valued used in check.
+   * @param message - Message for current permission.
+   * @returns {Rules} -
+   */
   can(status: boolean, message?: string): this {
     this.checkCurrentPermission();
     Object.assign(this.rules[this.current][this.currentPermission], {
@@ -64,6 +101,11 @@ export class Rules {
     return this;
   }
 
+  /**
+   * @description - Used for added a dynamic validation for a permission.
+   * @param cb - Callback used in check funcion for validation.
+   * @returns {Rules} -
+   */
   dynamic(cb: ValidatorFunctionType): this {
     this.checkCurrentPermission();
     Object.assign(this.rules[this.current][this.currentPermission], {
@@ -72,6 +114,10 @@ export class Rules {
     return this;
   }
 
+  /**
+   * @description - Added a message to current permission.
+   * @param message - Message.
+   */
   private message(message: string): void {
     this.checkCurrentPermission();
     Object.assign(this.rules[this.current][this.currentPermission], {
@@ -79,6 +125,13 @@ export class Rules {
     });
   }
 
+  /**
+   * @description - Function check used for validation.
+   * @param role - Role to find for test.
+   * @param permission - Permission t find for test.
+   * @param data - Data for dynamic permission.
+   * @returns {Array<boolean, string>} - Result of test.
+   */
   check(
     role: string | null,
     permission: string,
