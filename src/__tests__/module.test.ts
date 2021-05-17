ab850ff31474878c09c0c8f71d854fda387b273b
kikeztw <kikeztw@gmail.com>
2021-05-17 18:23:32 -0400

    reduced the complexity when creating rules

--- src/__tests__/module.test.ts
+++ src/__tests__/module.test.ts
@@ -1,13 +1,11 @@
 import { test, expect } from '@jest/globals';
-import { Rules } from '../rules';
+import { RBAC } from '../rbac';
 
 test('Test static rules', () => {
-  const staticRules = new Rules();
-  staticRules.role('admin').permission('dashboard').can(true, 'Access granted');
-  staticRules
-    .role('manager')
-    .permission('dashboard')
-    .can(false, 'Access denied');
+  const staticRules = new RBAC();
+  staticRules.createRule('admin', 'dashboard', true, 'Access granted');
+  staticRules.createRule('manager', 'dashboard', false, 'Access denied');
+
   expect(staticRules.check('admin', 'dashboard')).toStrictEqual([
     true,
     'Access granted',
@@ -19,16 +17,13 @@ test('Test static rules', () => {
 });
 
 test('Test dynamic rules', () => {
-  const dynamic = new Rules();
+  const dynamic = new RBAC();
   const testData = { id: 'test' };
-  dynamic
-    .role('admin')
-    .permission('dashboard')
-    .dynamic((data: any) => {
-      const result = data.id === testData.id;
-      const message = result ? 'Access granted' : 'Access denied';
-      return [result, message];
-    });
+  dynamic.createRule('admin', 'dashboard', (data: any) => {
+    const result = data.id === testData.id;
+    const message = result ? 'Access granted' : 'Access denied';
+    return [result, message];
+  });
   expect(dynamic.check('admin', 'dashboard', testData)).toStrictEqual([
     true,
     'Access granted',
@@ -39,18 +34,21 @@ test('Test dynamic rules', () => {
 });
 
 test('Test default rules', () => {
-  const rule = new Rules();
-  rule.role('admin').permission('dashboard').can(false);
+  const rule = new RBAC();
+  rule.createRule('admin', 'dashboard', false, 'Access granted');
   rule.setDefaultRole('admin');
-  expect(rule.check(null, 'dashboard')).toStrictEqual([false]);
+  expect(rule.check(null, 'dashboard')).toStrictEqual([
+    false,
+    'Access granted',
+  ]);
   expect(() => {
     rule.setDefaultRole('error-role');
   }).toThrow('Role not fund in rules');
 });
 
 test('Unexpected use of check', () => {
-  const rule = new Rules();
-  rule.role('admin').permission('dashboard').can(false);
+  const rule = new RBAC();
+  rule.createRule('admin', 'dashboard', false, 'Access granted');
   expect(rule.check(null, 'dashboard')).toStrictEqual([
     false,
     'Not default role or default-role found',
@@ -59,15 +57,4 @@ test('Unexpected use of check', () => {
     false,
     'Not permission error-role found in rules',
   ]);
-});
-
-test('Unexpected permission assignment', () => {
-  const rule = new Rules();
-
-  expect(() => {
-    rule.permission('dashboard');
-  }).toThrow('Role not defined');
-  expect(() => {
-    rule.role('admin').can(true);
-  }).toThrow('Permssion not defined');
 });
--- src/rbac.ts
+++ src/rbac.ts
@@ -2,28 +2,25 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { RulesType, ValidatorFunctionType } from './types';
 
-export class Rules {
+export class RBAC {
   private rules: RulesType = {};
-  private current = '';
-  private currentPermission = '';
   private currentRole = '';
 
-  /**
-   * @description - Check if the currently assigned role is within the rules.
-   */
-  private checkCurrentRole(): void {
-    if (!this.rules[this.current]) {
-      throw new Error('Role not defined');
-    }
-  }
-
-  /**
-   * @description -  Check if the currently assigned permission is within the current role.
-   */
-  private checkCurrentPermission(): void {
-    if (!this.rules[this.current][this.currentPermission]) {
-      throw new Error('Permssion not defined');
-    }
+  createRule(
+    roleName: string,
+    permissionName: string,
+    test: boolean | ValidatorFunctionType,
+    message?: string,
+  ): void {
+    Object.assign(this.rules, {
+      [roleName]: {
+        [permissionName]: {
+          can: typeof test === 'boolean' ? test : undefined,
+          dynamic: typeof test === 'function' ? test : undefined,
+          message,
+        },
+      },
+    });
   }
 
   /**
@@ -55,74 +52,6 @@ export class Rules {
     } else {
       throw new Error('Role not fund in rules');
     }
-  }
-
-  /**
-   * @description - Added a role to rules.
-   * @param name - Role name.
-   * @returns {Rules} -
-   */
-  role(name: string): this {
-    if (!this.isRoleInRules(name)) {
-      Object.assign(this.rules, { [name]: {} });
-    }
-    this.current = name;
-    return this;
-  }
-
-  /**
-   * @description - Added a permission to a role.
-   * @param name - Permission name.
-   * @returns {Rules} -
-   */
-  permission(name: string): this {
-    this.checkCurrentRole();
-    if (!this.isPermissionInRole(this.current, name)) {
-      Object.assign(this.rules[this.current], { [name]: {} });
-    }
-    this.currentPermission = name;
-    return this;
-  }
-
-  /**
-   * @description - Used for added validation for static permission.
-   * @param status - Valued used in check.
-   * @param message - Message for current permission.
-   * @returns {Rules} -
-   */
-  can(status: boolean, message?: string): this {
-    this.checkCurrentPermission();
-    Object.assign(this.rules[this.current][this.currentPermission], {
-      can: status,
-    });
-    if (message) {
-      this.message(message);
-    }
-    return this;
-  }
-
-  /**
-   * @description - Used for added a dynamic validation for a permission.
-   * @param cb - Callback used in check funcion for validation.
-   * @returns {Rules} -
-   */
-  dynamic(cb: ValidatorFunctionType): this {
-    this.checkCurrentPermission();
-    Object.assign(this.rules[this.current][this.currentPermission], {
-      dynamic: cb,
-    });
-    return this;
-  }
-
-  /**
-   * @description - Added a message to current permission.
-   * @param message - Message.
-   */
-  private message(message: string): void {
-    this.checkCurrentPermission();
-    Object.assign(this.rules[this.current][this.currentPermission], {
-      message,
-    });
   }
 
   /**
