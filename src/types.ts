b3d8de2984cecb1b5b5a2c51cff4c96bccce55a4
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2021-05-28 09:51:34 -0400

    force enums

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,24 @@
-export type RulesType<R,P> = {
-  [role in keyof R]: {
-    [permission in keyof P]: {
+// export type RulesType<R,P> = {
+//   [role in keyof R]: {
+//     [permission in keyof P]: {
+//       can?: boolean;
+//       dynamic?: ValidatorFunctionType;
+//       message?: string;
+//     };
+//   };
+// };
+
+export type RulesType<R extends string, P extends string> = Record<
+  R,
+  Record<
+    P,
+    {
       can?: boolean;
       dynamic?: ValidatorFunctionType;
       message?: string;
-    };
-  };
-};
-
+    }
+  >
+>;
 export type ValidatorFunctionType<T = unknown> = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: T,
